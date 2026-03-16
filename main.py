555a2719d356482ec73dc5fef111e184d1edcccb
Makoto-online <milciadescandefe@gmail.com>
2026-03-16 17:31:30 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+meme_dict = {
+            "CRINGE": "Algo excepcionalmente raro o embarazoso",
+            "LOL": "Una respuesta común a algo gracioso",
+            "XD": "Emoji que representa risa interminable"
+            }
+
+print("Palabras en el diccionario", meme_dict.keys())
+word = input("Ingresa la Palabra que quieras ingresar:").upper()#upper es para que todo lo que este en minusculla se ponga a mayuscula de misma forma funciona el lower
+
+if word in meme_dict.keys():
+    print(meme_dict[word])
+else:
+    print("Esa palabra no ha sido añadida en este diccionario")
